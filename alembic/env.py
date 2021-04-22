45e2e46e354515fb2dcfadf5b117bec43f9f9801
Tarik Stupac <tarik.stupac@zendev.se>
2021-04-22 14:29:14 +0200

    Changes to alembic ini and env.py file to get Database url from env variable.

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+import os
 
 from alembic import context
 from models.base import Base
@@ -25,6 +26,8 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_url():
+    return os.getenv("DATABASE_URL")
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -38,7 +41,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,8 +60,10 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration['sqlalchemy.url'] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
