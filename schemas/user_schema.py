64be46c93c38df7cf687f783783cd0509f11af8c
Tarik Stupac <tarik.stupac@zendev.se>
2021-04-27 15:58:17 +0200

    Bug fixes for the user/me route.

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -9,10 +9,10 @@ class User(BaseModel):
     status : int
     first_name: constr(min_length=1, max_length=50)
     last_name: constr(min_length=1, max_length=50)
-    phone: constr(regex='^\+(?:[0-9] ?){6,14}[0-9]$')
+    phone: Optional[constr(regex='^\+(?:[0-9] ?){6,14}[0-9]$')]
     flag : Optional[str]
     map_style : Optional[int]
-    display_name: constr(min_length=1, max_length=20)
+    display_name: Optional[constr(min_length=1, max_length=20)]
     country_id : str 
     country : Country
 
