253f11973bbd930ca59509730307467f3a0924b3
Tarik Stupac <tarik.stupac@zendev.se>
2021-04-28 13:43:16 +0200

    default for flag

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -29,7 +29,7 @@ class UserCreate(BaseModel):
     first_name: constr(min_length=1, max_length=50)
     last_name: constr(min_length=1, max_length=50)
     phone: Optional[constr(regex='^\+(?:[0-9] ?){6,14}[0-9]$')]
-    flag : Optional[str]
+    flag : Optional[str] = 'BA'
     map_style : Optional[int]
     display_name: Optional[constr(min_length=1, max_length=20)]
     country_id : str 
