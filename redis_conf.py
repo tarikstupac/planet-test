80bcb70a9d2a8870294fae556d975e95af6a7a6e
Tarik Stupac <tarik.stupac@zendev.se>
2021-05-19 15:45:04 +0200

    Fix for heroku redis for contact page caching.

--- redis_conf.py
+++ redis_conf.py
@@ -5,7 +5,7 @@ REDIS_URL = os.environ.get("REDIS_URL", None)
 if REDIS_URL is not None:
     token_watcher = redis.from_url(REDIS_URL, db=1)
     country_watcher = redis.from_url(REDIS_URL, db=0)
-    contact_cache = redis.from_url(REDIS_URL, db=2)
+    contact_cache = redis.from_url(REDIS_URL, db=1)
 else:
     token_watcher = redis.Redis(db=1)
     country_watcher = redis.Redis(host='127.0.0.1', port=9851, db=0)
